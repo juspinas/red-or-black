0cec368c44d1163c65c0aa2811988e55805580d3
Josh <josh.espinas@xero.com>
2019-08-23 23:09:18 +1200

    Refactors file path

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
+const htmlPath = path.join(__dirname, "index.html");
+
 app.get("/", (req, res) => {
-	res.sendFile(path.join(__dirname, "index.html"));
+	res.sendFile(htmlPath);
 });
 
 console.log("dirname = ", __dirname);
