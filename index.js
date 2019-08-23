d53166572d042ced08e165297148c08e8d1c47c6
juspinas <justin.espinas@gmail.com>
2019-08-24 00:35:19 +1200

    Adds new /newcard endpoint that requests api with given deck_id

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ app.get("/", (req, res) => {
 	res.sendFile(htmlPath);
 });
 
-console.log("dirname = ", __dirname);
-console.log("path join = ", path.join(__dirname, "index.html"));
-
 // app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
 app.get("/cards", async (req, res) => {
@@ -35,5 +32,28 @@ app.get("/cards", async (req, res) => {
         remaining: apiResponseObj.remaining
     });
 });
+
+app.get("/newcard", async (req, res) => {
+    const deck_id = "8fmprl9v8xc2";
+    var settings = {
+        async: true,
+        crossDomain: true,
+        url: "https://deckofcardsapi.com/api/deck/" + deck_id +"/draw/?count=1",
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+    const apiResponse = await request(settings).catch(error => console.log(error));
+
+    const apiResponseObj = JSON.parse(apiResponse);
+
+    res.status(200).send({
+        success: apiResponseObj.success,
+        cards: apiResponseObj.cards,
+        deck_id: apiResponseObj.deck_id,
+        remaining: apiResponseObj.remaining
+    });
+})
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
