a32830daaa218ed517d8b7eaf4914f56a789f2ec
Josh <josh.espinas@xero.com>
2019-08-24 00:11:06 +1200

    Has small tabbing format changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const express = require("express");
-const request = require("request-promise");
+const requestPromise = require("request-promise");
 const app = express();
 
 const htmlPath = path.join(__dirname, "index.html");
@@ -15,25 +15,27 @@ console.log("path join = ", path.join(__dirname, "index.html"));
 // app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
 app.get("/cards", async (req, res) => {
-    var settings = {
-        async: true,
-        crossDomain: true,
-        url: "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1",
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json"
-        }
-    };
-    const apiResponse = await request(settings).catch(error => console.log(error));
+	var settings = {
+		async: true,
+		crossDomain: true,
+		url: "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1",
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json"
+		}
+	};
+	const apiResponse = await request(settings).catch(error =>
+		console.log(error)
+	);
 
-    const apiResponseObj = JSON.parse(apiResponse);
+	const apiResponseObj = JSON.parse(apiResponse);
 
-    res.status(200).send({
-        shuffled: apiResponseObj.shuffled,
-        success: apiResponseObj.success,
-        deck_id: apiResponseObj.deck_id,
-        remaining: apiResponseObj.remaining
-    });
+	res.status(200).send({
+		shuffled: apiResponseObj.shuffled,
+		success: apiResponseObj.success,
+		deck_id: apiResponseObj.deck_id,
+		remaining: apiResponseObj.remaining
+	});
 });
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
